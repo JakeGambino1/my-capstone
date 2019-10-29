83f6a67a1ba8ed8465681711d2e9e78b52b4db02
Jacob Gambino <marketingboon@Jacobs-MacBook-Pro.local>
2019-10-29 16:02:48 -0500

    post request reaches server for users, profile, written-content, and auth

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/written-content', require('./routes/api/written-content'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 3000;
 
