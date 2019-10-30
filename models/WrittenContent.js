758c3e6f1296e71e1f506dcc67a2d0affcd79ce1
Jacob Gambino <marketingboon@Jacobs-MacBook-Pro.local>
2019-10-30 07:11:01 -0500

    can create new written content and post it to MongoDB atlas

--- models/WrittenContent.js
+++ models/WrittenContent.js
@@ -1,15 +1,48 @@
 const mongoose = require('mongoose');
 
 const WrittenContentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
   title: {
     type: String,
     required: true
   },
   content: {
-    type: String
+    type: String,
+    required: true
   },
   date: {
     type: Date,
+    default: Date.now
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: String,
     default: Date.now
   }
 });
--- routes/api/written-content.js
+++ routes/api/written-content.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const WrittenContent = require('../../models/WrittenContent');
+const Profile = require('../../models/Profile');
 
-router.get('/', (req, res) => res.send('Written-content route'));
+// POST api/written-content/
+
+router.post(
+  '/',
+  [
+    auth,
+    check('title', 'You must have a title.')
+      .not()
+      .isEmpty(),
+    check('content', 'Your content cannot be blank.')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new WrittenContent({
+        user: req.user.id,
+        title: req.body.title,
+        content: req.body.content
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json('Server Error');
+    }
+  }
+);
 
 module.exports = router;
