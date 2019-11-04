f0a4480b466569d1fe0386277fe02f4bad25f085
Jacob Gambino <marketingboon@Jacobs-MacBook-Pro.local>
2019-11-04 07:45:53 -0600

    updated main logo positioning and layout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,35 +3,37 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className="navbar-fixed">
+    <nav className="navbar-fixed">
       <nav>
-        <div className="nav-wrapper">
-          <Link to="#" className="brand-logo">
-            Logo
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/createpost">Create Post</Link>
-            </li>
-            <li>
-              <Link to="/actionlist">Action List</Link>
-            </li>
-            <li>
-              <Link to="/createprofile">Create Profile</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
+        <div className="container">
+          <div className="nav-wrapper">
+            <Link to="#" className="brand-logo">
+              Staff Engagement
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/createpost">Create Post</Link>
+              </li>
+              <li>
+                <Link to="/actionlist">Action List</Link>
+              </li>
+              <li>
+                <Link to="/createprofile">Create Profile</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-    </div>
+    </nav>
   );
 };
 
